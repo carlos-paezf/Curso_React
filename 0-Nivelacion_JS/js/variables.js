14ae3c264810a6d9a724aa07652e5b79e0a939f7
Carlos David Páez Ferreira <carlos.paezf@usantoto.edu.co>
2021-08-10 13:54:42 -0500

    Variables

---
+++ 0-Nivelacion_JS/js/variables.js
@@ -0,0 +1,8 @@
+var nombreVar = "David Ferrer";
+document.write(nombreVar + "<br>");
+
+let nombreLet = "Ferrer";
+document.write(nombreLet + "<br>");
+
+const nombreConst = "David";
+document.write(nombreConst + "<br>");
