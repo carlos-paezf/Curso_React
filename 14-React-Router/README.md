5404a12d1192c4a66a8d75d428ed658e1f63679a
Carlos David Páez Ferreira <carlos.paezf@usantoto.edu.co>
2021-09-22 21:04:08 -0500

    Configuración del proyecto

--- 14-React-Router/README.md
+++ 14-React-Router/README.md
@@ -30,3 +30,15 @@ En cada componente, se aplican clases de Bootstrap para tener una apariencia bá
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 ```
+
+## Archivos adicionales para el proyecto
+
+Dentro de la carpeta `public/assets/` encontramos algunas imágenes que nos serviran como contenido para nuestra aplicación. También hay un archivo dentro de la carpeta `models` llamado `foods`, el cual contiene un arreglo de objetos con lo que asociaremos más adelante las imágenes antes mencionadas.
+
+## Instalación de la librería `react-router-dom`
+
+Para instalar la librería hice uso del comando:
+
+```cmd
+yarn add react-router-dom
+```
---
+++ 14-React-Router/router/src/models/foods.js
@@ -0,0 +1,98 @@
+export const Foods = [
+    {
+        id: 'arroz',
+        nombre: 'Arroz con Pollo',
+        descripcion: 'Un rico arroz amarillo con pollo desmenuzado',
+        categoria: 'A'
+    },
+    {
+        id: 'brownie',
+        nombre: 'Brownie de Chocolate',
+        descripcion: 'Brownies de chocolate amargo',
+        categoria: 'B'
+    },
+    {
+        id: 'burritos',
+        nombre: 'Burritos Mexicanos',
+        descripcion: 'Burritos con tortilla de maíz y carne fina',
+        categoria: 'A'
+    },
+    {
+        id: 'carne-asada',
+        nombre: 'Carne Asada',
+        descripcion: 'Carne Asada con la calidad A5',
+        categoria: 'A'
+    },
+    {
+        id: 'chocolate',
+        nombre: 'Chocolate',
+        descripcion: 'Chocolate preparado con cacao Colombiano',
+        categoria: 'B'
+    },
+    {
+        id: 'donas',
+        nombre: 'Donas',
+        descripcion: 'Donas con chocolate blanco y chispas azucaradas',
+        categoria: 'B'
+    },
+    {
+        id: 'ensalada',
+        nombre: 'Ensalada con Pollo',
+        descripcion: 'Ensalada mixta de pollo y verduras',
+        categoria: 'A'
+    },
+    {
+        id: 'hamburguesa',
+        nombre: 'Hamburguesa triple carne',
+        descripcion: 'Hamburguesa triple carne y queso derretido',
+        categoria: 'A'
+    },
+    {
+        id: 'helado',
+        nombre: 'Helado',
+        descripcion: 'Helado preparado con la leche de la más alta calidad',
+        categoria: 'B'
+    },
+    {
+        id: 'hot-dog',
+        nombre: 'Hot-Dog',
+        descripcion: 'Hot Dog tipico de los puestos de comida americanos',
+        categoria: 'A'
+    },
+    {
+        id: 'm&m',
+        nombre: 'M&M',
+        descripcion: 'Pepitas de chocolate con cobertura colorida',
+        categoria: 'B'
+    },
+    {
+        id: 'pizza',
+        nombre: 'Pizza Italiana',
+        descripcion: 'Pizza con masa Italiana horneada en leña',
+        categoria: 'A'
+    },
+    {
+        id: 'pollo',
+        nombre: 'Pollo Frito',
+        descripcion: 'Pollo frito con cobertura crujiente',
+        categoria: 'A'
+    },
+    {
+        id: 'tacos',
+        nombre: 'Tacos Mexicanos',
+        descripcion: 'Tacos preparados con carne de res',
+        categoria: 'A'
+    },
+    {
+        id: 'torta',
+        nombre: 'Torta',
+        descripcion: 'Torta de 1/2 libra para fiesta',
+        categoria: 'B'
+    },
+    {
+        id: 'turron',
+        nombre: 'Turron',
+        descripcion: 'Turron con crispis de arroz',
+        categoria: 'B'
+    },
+]
